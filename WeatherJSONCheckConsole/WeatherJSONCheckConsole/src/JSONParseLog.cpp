cde7c039f35ad3848c4fdb67c235e01db3556dd8
OlegLobko <sledge@ukr.net>
2015-10-06 19:54:32 +0300

    Added the library - picojson.h (BSD License)

---
+++ WeatherJSONCheckConsole/WeatherJSONCheckConsole/src/JSONParseLog.cpp
@@ -0,0 +1,9 @@
+//
+//  JSONParseLog.cpp
+//  WeatherJSONCheckConsole
+//
+//  Created by Admin on 06.10.15.
+//  Copyright (c) 2015 weather. All rights reserved.
+//
+
+#include "JSONParseLog.h"
---
+++ WeatherJSONCheckConsole/WeatherJSONCheckConsole/src/JSONParseLog.h
@@ -0,0 +1,14 @@
+//
+//  JSONParseLog.h
+//  WeatherJSONCheckConsole
+//
+//  Created by Admin on 06.10.15.
+//  Copyright (c) 2015 weather. All rights reserved.
+//
+
+#ifndef __WeatherJSONCheckConsole__JSONParseLog__
+#define __WeatherJSONCheckConsole__JSONParseLog__
+
+#include <stdio.h>
+
+#endif /* defined(__WeatherJSONCheckConsole__JSONParseLog__) */
